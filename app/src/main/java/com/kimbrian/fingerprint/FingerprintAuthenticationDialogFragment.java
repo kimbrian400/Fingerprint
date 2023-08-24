23250ab83d605850a2bb266d9b5a0d0647c7b210
KimBrian <kimbrian400@gmail.com>
2023-08-24 04:05:18 +0300

    1.0

---
+++ app/src/main/java/com/kimbrian/fingerprint/FingerprintAuthenticationDialogFragment.java
@@ -0,0 +1,6 @@
+package com.kimbrian.fingerprint;
+
+import androidx.fragment.app.DialogFragment;
+
+public class FingerprintAuthenticationDialogFragment extends DialogFragment {
+}
--- app/src/main/java/com/kimbrian/fingerprint/MainActivity.java
+++ app/src/main/java/com/kimbrian/fingerprint/MainActivity.java
@@ -3,128 +3,54 @@ package com.kimbrian.fingerprint;
 import android.Manifest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.hardware.fingerprint.FingerprintManagerCompat;
+import androidx.biometric.BiometricPrompt;
 
-import android.content.pm.PackageManager;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.CancellationSignal;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
 
-    private static final int REQUEST_BIOMETRIC_PERMISSION = 100;
-    private ImageView fingerprintIcon;
-    private TextView unlockText;
-    private ImageView animationView;
-    private FingerprintManagerCompat fingerprintManager;
-    private CancellationSignal cancellationSignal;
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RelativeLayout layout = findViewById(R.id.layout);
-        fingerprintIcon = findViewById(R.id.fingerprintIcon);
-        unlockText = findViewById(R.id.unlockText);
-        animationView = findViewById(R.id.animationView);
+        // Initialize executor
+        executor = Executors.newSingleThreadExecutor();
 
-        // Initialize FingerprintManagerCompat
-        fingerprintManager = FingerprintManagerCompat.from(this);
-
-        // Check for biometric hardware and permission
-        if (!fingerprintManager.isHardwareDetected() || ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED) {
-            // Biometric authentication is not available, handle this case accordingly
-            // You can hide the fingerprint UI and provide an alternative method for authentication
-            fingerprintIcon.setVisibility(View.GONE);
-            unlockText.setText("Biometric not available");
-        } else {
-            // Request the biometric permission
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.USE_BIOMETRIC}, REQUEST_BIOMETRIC_PERMISSION);
-            }
-        }
-
-        // Set an onClickListener for the fingerprint icon to start the animation and authenticate
-        fingerprintIcon.setOnClickListener(new View.OnClickListener() {
+        // Create a BiometricPrompt instance
+        biometricPrompt = new BiometricPrompt(this, executor, new BiometricPrompt.AuthenticationCallback() {
             @Override
-            public void onClick(View v) {
-                startUnlockAnimation();
-                startFingerprintAuthentication();
-            }
-        });
-    }
-
-    private void startUnlockAnimation() {
-        animationView.setVisibility(View.VISIBLE);
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fingerprint_animation);
-        animationView.startAnimation(animation);
-    }
-
-    private void startFingerprintAuthentication() {
-        // Create a cancellation signal to stop authentication if needed
-        cancellationSignal = new CancellationSignal();
-
-        // Create a callback for fingerprint authentication
-        FingerprintManagerCompat.AuthenticationCallback authenticationCallback = new FingerprintManagerCompat.AuthenticationCallback() {
-            @Override
-            public void onAuthenticationError(int errMsgId, CharSequence errString) {
-                // Handle authentication errors here
-                // You can display an error message or take appropriate action
-            }
-
-            @Override
-            public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
-                // Handle authentication help messages (e.g., "Move your finger")
-            }
-
-            @Override
-            public void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result) {
-                // Fingerprint authentication succeeded, you can unlock the account or perform your action
-                // You can also dismiss the animation here
-                animationView.clearAnimation();
-                animationView.setVisibility(View.INVISIBLE);
-
-                // Handle the successful authentication
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                // Fingerprint authentication succeeded; allow access to the app.
+                // You can put your app's logic here.
             }
 
             @Override
             public void onAuthenticationFailed() {
-                // Handle authentication failure (e.g., wrong fingerprint)
+                super.onAuthenticationFailed();
+                // Fingerprint authentication failed.
+                // Handle this case as needed.
             }
-        };
+        });
 
-        // Start fingerprint authentication
-        fingerprintManager.authenticate(null, 0, cancellationSignal, authenticationCallback, null);
-    }
+        // Configure the authentication prompt
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Fingerprint Authentication")
+                .setSubtitle("Unlock the app with your fingerprint")
+                .setNegativeButtonText("Cancel")
+                .build();
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // Cancel the fingerprint authentication when the activity is stopped
-        if (cancellationSignal != null) {
-            cancellationSignal.cancel();
-            cancellationSignal = null;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_BIOMETRIC_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, you can proceed with fingerprint authentication
-            } else {
-                // Permission denied, handle this case accordingly
-                fingerprintIcon.setVisibility(View.GONE);
-                unlockText.setText("Permission denied for biometric");
-            }
-        }
+        // Trigger the fingerprint authentication dialog
+        findViewById(R.id.authenticateButton).setOnClickListener(view -> {
+            biometricPrompt.authenticate(promptInfo);
+        });
     }
 }
