ecc2e03ae8bd98834a69faa32fe2d2423941a095
KimBrian <kimbrian400@gmail.com>
2023-08-24 09:24:20 +0300

    1.0

--- app/src/main/java/com/kimbrian/fingerprint/MainActivity.java
+++ app/src/main/java/com/kimbrian/fingerprint/MainActivity.java
@@ -9,7 +9,9 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Executor executor;
@@ -31,13 +33,22 @@ public class MainActivity extends AppCompatActivity {
                 super.onAuthenticationSucceeded(result);
                 // Fingerprint authentication succeeded; allow access to the app.
                 // You can put your app's logic here.
-            }
+                // Start the HomeActivity upon successful authentication
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                  startActivity(intent);
+                    finish(); // Optionally finish the MainActivity if you don't want to go back to it.
+                }
 
             @Override
             public void onAuthenticationFailed() {
                 super.onAuthenticationFailed();
                 // Fingerprint authentication failed.
-                // Handle this case as needed.
+                    // Display a message to the user indicating the failure
+                    Toast.makeText(MainActivity.this, "Fingerprint authentication failed. Please try again.", Toast.LENGTH_SHORT).show();
+                // Launch the PIN input activity
+                Intent intent = new Intent(MainActivity.this, PinInputActivity.class);
+                startActivity(intent);
+                finish(); // Optionally finish the MainActivity if you don't want to go back to it.
             }
         });
 
